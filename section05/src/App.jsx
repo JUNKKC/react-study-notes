dffeb93f62c0c1cc649e8a990170cd4fb8de7a45
kimchanjun <rlackswns7@gmail.com>
2024-08-19 00:59:46 +0900

    주말

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 
+import Register from "./components/Register";
+
 function App() {
   return (
     <>
-      <h1>안녕 리액트!</h1>
+      <Register />
     </>
   );
 }
---
+++ section05/src/components/Register.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+// 간단한 회원다입 폼
+// 1. 이름
+// 2. 생년월일
+// 3. 국적
+// 4. 자기소개
+
+const Register = () => {
+  const [input, setInput] = useState({
+    name: "",
+    birth: "",
+    country: "",
+    bio: "",
+  });
+
+  
+  const onChangeName = (e) => {
+    setInput({
+      ...input,
+      name: e.target.value,
+    });
+  };
+
+  const onChangeBirth = (e) => {
+    setInput({
+      ...input,
+      birth: e.target.value,
+    });
+  };
+
+  const onChangeCountry = (e) => {
+    setInput({
+      ...input,
+      country: e.target.value,
+    });
+  };
+  const onChangeBio = (e) => {
+    setInput({
+      ...input,
+      bio: e.target.value,
+    });
+  };
+  return (
+    <div>
+      <div>
+        <input
+          value={input.name}
+          onChange={onChangeName}
+          placeholder={"이름"}
+        />
+      </div>
+
+      <div>
+        <input value={input.birth} onChange={onChangeBirth} type="date" />
+      </div>
+      <div>
+        <select value={input.country} onChange={onChangeCountry}>
+          <option>-</option>
+          <option>한국</option>
+          <option>미국</option>
+          <option>영국</option>
+        </select>
+      </div>
+      <div>
+        <textarea value={input.bio} onChange={onChangeBio} />
+      </div>
+    </div>
+  );
+};
+
+export default Register;
